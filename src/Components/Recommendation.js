9442ef049d1501c8d6e9be9f3d43dc5e9eed2052
Verhóczki Márton <verhoczki.marci@gmail.com>
2020-01-05 13:41:31 +0100

    Implement Recommendation component

--- src/Components/Recommendation.js
+++ src/Components/Recommendation.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Card from "react-bootstrap/Card";
 
-const Recommendation = () => {
-    return ( null );
+const Recommendation = ({rating, comment}) => {
+    return (
+        <Card style={{ width: "80%" }} className="text-center">
+            <Card.Header>{rating}⭐</Card.Header>
+            <Card.Body>
+                <Card.Text>
+                    {comment}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
 }
  
 export default Recommendation;
