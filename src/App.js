1bfed947031f2cc59fc423eaa52083072099c387
Verhóczki Márton <verhoczki.marci@gmail.com>
2020-01-05 13:31:26 +0100

    Minor changes in routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import IndexPage from "./Components/IndexPage";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import DetailsPage from "./Components/DetailsPage";
 
 function App() {
   return (
       <Router>
           <div className="App">
-              <Route exact path="/" component={IndexPage}></Route>
-
+              <Route exact path="/" component={IndexPage}/>
+              <Route path="/video/:id" render={(props) => <DetailsPage {...props} /> }/>
           </div>
       </Router>
   );
--- src/Components/VideoPreview.js
+++ src/Components/VideoPreview.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Card from "react-bootstrap/Card";
-
+import {Link} from "react-router-dom";
+import DetailsPage from "./DetailsPage";
 const VideoPreview = ({id, name}) => {
     return (
-        <Card style={{ width: '18rem' }}>
+        <Link to={'/video/' + id}><Card style={{ width: '18rem' }}>
             <Card.Body>
                 <Card.Title>{name}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{id}</Card.Subtitle>
             </Card.Body>
-        </Card>
+        </Card></Link>
     );
 }
  
